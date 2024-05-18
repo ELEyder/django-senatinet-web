b0e4311463c58498ac04821cee21a54805b60eb7
Eyder <74992528+ELEyder@users.noreply.github.com>
2024-05-17 20:42:56 -0500

    no database

--- senatiweb/settings.py
+++ senatiweb/settings.py
@@ -83,18 +83,18 @@ WSGI_APPLICATION = 'senatiweb.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    "default": {
-        "ENGINE": os.environ.get("ENGINE"),
-        "NAME": os.environ.get("NAME"),
-        "USER": os.environ.get("USER"),
-        "PASSWORD": os.environ.get("PASSWORD"),
-        "HOST": os.environ.get("HOST"),
-        "PORT": os.environ.get("PORT"),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # "default": {
+    #     "ENGINE": os.environ.get("ENGINE"),
+    #     "NAME": os.environ.get("NAME"),
+    #     "USER": os.environ.get("USER"),
+    #     "PASSWORD": os.environ.get("PASSWORD"),
+    #     "HOST": os.environ.get("HOST"),
+    #     "PORT": os.environ.get("PORT"),
+    # }
 }
 
 
