35bb18a8c66cfbd85b962cee8fcd885e4df6243a
Eyder <74992528+ELEyder@users.noreply.github.com>
2024-05-17 22:25:39 -0500

    like repared

--- postapp/views.py
+++ postapp/views.py
@@ -54,13 +54,13 @@ def like(request, id_post):
     idAuth = request.session.get('user_id')
     userLogin = DefaultUser.getUserById(idAuth)
     post = Post.getPostById(id_post)
-    if (user['id'] in post['likesD']):
+    if (userLogin['id'] in post['likesD']):
         likes = int(post['likes']) - 1
-        Post.updateLike(id_post, user['id'], likes, -1)
+        Post.updateLike(id_post, userLogin['id'], likes, -1)
         return HttpResponse("¡Publicación unliked!")
 
     else:
         likes = int(post['likes']) + 1
-        Post.updateLike(id_post, user['id'], likes, 1)
+        Post.updateLike(id_post, userLogin['id'], likes, 1)
         return HttpResponse("¡Publicación liked!")
 
