f1f82140486c45c836ecd817a9aaf9645f3b100d
ELEyder <74992528+ELEyder@users.noreply.github.com>
2024-05-25 10:03:11 -0500

    Chat design

--- chatapp/models.py
+++ chatapp/models.py
@@ -11,12 +11,24 @@ class Chat():
         for chat in chats_data:
             chat_data = chat.to_dict()
             chat_data['id'] = chat.id
+            if len(chat_data['members']) == 2:
+                chat_data['sender'] = id
+                if chat_data['members'][0] == id:
+                    receiverId = chat_data['members'][1]
+                else:
+                    receiverId = chat_data['members'][0]
+                receiver = db.collection('users').document(receiverId).get().to_dict()
+                chat_data['receiver'] = receiverId
+                chat_data['receiverUsername'] = receiver['username']
+                chat_data['receiverFirstName'] = receiver['firstName']
+                chat_data['receiverLastName'] = receiver['lastName']
+                chat_data['receiverUrlAvatar'] = receiver['urlAvatar']
             chats.append(chat_data)
         return chats
     
     @staticmethod
     def getMessagesById(id):         
-        messages_data = db.collection(f'chats/{id}/messages').stream()
+        messages_data = db.collection(f'chats/{id}/messages').order_by('date', direction=firestore.Query.DESCENDING).stream()
         messages = []
         for message in messages_data:
             message_data = message.to_dict()
--- static/js/chat/messages.js
+++ static/js/chat/messages.js
@@ -17,13 +17,23 @@ function loadMessages(id){
     db.collection(`chats/${id}/messages`)
         .onSnapshot(snapshot => {
             const messagesDiv = document.getElementById('chat-details');
-            messagesDiv.innerHTML = '<h2>Mensajes del Chat</h2><ul>';
+            var receiverName = document.getElementById('receiverFullName').innerText
+            messagesDiv.innerHTML = `
+            <p>${receiverName}</p>
+            `;
             
             snapshot.forEach(doc => {
                 const message = doc.data();
-                messagesDiv.innerHTML += `<li><strong>${message.author}:</strong> ${message.content} <br><small>${message.date}</small></li>`;
-            });
+                messagesDiv.innerHTML += `
+                <div class="message">
 
+                <strong>${message.author}:</strong>
+                ${message.content} <br>
+                </div>
+                `;
+                // <small>${message.date}</small>
+            });
+            
             messagesDiv.innerHTML += '</ul>';
         }, error => {
             console.error('Error al cargar los mensajes:', error);
@@ -33,8 +43,9 @@ function loadMessages(id){
 function sendMensaje(author){
     const csrfToken = document.querySelector('[name="csrfmiddlewaretoken"]').value;
     const dominioBase = window.location.origin;
-    const id = document.getElementById('button-chat').getAttribute('chatId');
-    var content = document.getElementById('message-input').value
+    const id = document.getElementById('chat-details').getAttribute('idChat');
+    var input_content = document.getElementById('message-input')
+    var content = input_content.value
     const messageData = {
         author: author,
         content: content,
@@ -49,7 +60,9 @@ function sendMensaje(author){
     })
     .then(response => {
         console.log('Respuesta del servidor:', response);
+        input_content.value = ""
         loadMessages(id);
+
     })
     .catch(error => {
         console.error('Error al enviar el mensaje:', error);
